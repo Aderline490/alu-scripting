af6ae001a7b37d277bebf19cc5911dba08d9a217
Aderline490 <gashugiaderline@gmail.com>
2025-07-21 11:05:38 +0200

    api advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""This module queries the Reddit API to fetch the top 10 hot posts of a subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyRedditClient/1.0'}
-    params = {'limit': 10}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
-
-        posts = response.json().get('data', {}).get('children', [])
-        if not posts:
-            print(None)
-            return
-
-        for post in posts:
-            print(post['data']['title'])
-
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
         print(None)
